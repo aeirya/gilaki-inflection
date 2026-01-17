2a228b5aaa35e033180b32b34818c9b897ed7eb3
aeirya <aeirya@gmail.com>
2026-01-17 06:34:25 +0100

    update infinitives list, fix read infinitive table function

--- gltk/verbs.py
+++ gltk/verbs.py
@@ -10,11 +10,15 @@ def read_table(path):
         ]
     return table
 
-def read_verb_table():
+def read_verb_infinitive2past_table():
     return read_table('gltk/infinitives-a.txt')
 
 def read_verb_past2present_table():
     return read_table('gltk/past-present.txt')
+
+def infinitive_past_lists():
+    table = read_verb_infinitive2past_table()
+    return [v[0] for v in table], [v[1] for v in table]
 
 def past_present_lists():
     table = read_verb_past2present_table()
---
+++ verb_inflation.py
@@ -0,0 +1,18 @@
+from gltk.verbs import infinitive_past_lists
+
+from pyfoma import *
+fsts = {}
+
+infinitives, pasts = infinitive_past_lists()
+fsts['verbs'] = FST.re('|'.join(infinitives))
+
+fsts['suffixes'] = FST.re(
+    "( '[Present]' | '[Past]' )" +\
+    "( '[Sg]' | '[Pl]' )" +\
+    "( '[1p]' | '[2p]' | '[3p]' )"
+    )
+
+fsts['lexicon'] = FST.re('$verbs $suffixes', fsts)
+fsts['grammar'] = FST.re('$lexicon', fsts)
+
+print(Paradigm(fsts['grammar'], ".*"))
