18abf889c791904abaef4fb3e2ce4e3708d4f1db
aeirya <aeirya@gmail.com>
2026-01-17 17:50:31 +0100

    add example for how to evaluate verb person stem endings

---
+++ verb_person.py
@@ -0,0 +1,64 @@
+from gltk.verb_table import read_full_verb_table
+from pyfoma import *
+
+fsts = {}
+
+
+table = read_full_verb_table().sample(1)
+
+fsts['present_stem_vocab'] = FST.re('|'.join(table['pres']))
+fsts['past_stem_vocab'] = FST.re('|'.join(table['past']))
+fsts['inf_vocab'] = FST.re('|'.join(table['inf']))
+
+fsts['vocab'] = FST.re('$inf_vocab', fsts)
+
+fsts['person_tag'] = FST.re(
+    "( '[1p,Sg]' | '[2p,Sg]' | '[3p,Sg]' | '[1p,Pl]' | '[2p,Pl]' | '[3p,Pl]' )"
+)
+fsts['tense_tag'] = FST.re(
+    "( '[Present]' | '[Past]' | '[PastCont]')"
+)
+
+fsts['tags'] = FST.re(''.join([
+    "$tense_tag",
+    "$person_tag",
+    ]), fsts)
+
+fsts['present_person_ending'] = FST.re(
+    "|".join([
+        "('[1p,Sg]'):(əm)",
+        "('[2p,Sg]'):(i)",
+        "('[3p,Sg]'):(e)",
+        "('[1p,Pl]'):(im)",
+        "('[2p,Pl]'):(id)",
+        "('[3p,Pl]'):(id)",
+    ]))
+
+fsts['past_person_ending'] = FST.re(
+    "|".join([
+        "('[1p,Sg]'):(əm)",
+        "('[2p,Sg]'):(i)",
+        "('[3p,Sg]'):(ə)",
+        "('[1p,Pl]'):(im)",
+        "('[2p,Pl]'):(id)",
+        "('[3p,Pl]'):(id)",
+    ]))
+
+fsts['past_cont_person_ending'] = FST.re(
+    "|".join([
+        "('[1p,Sg]'):(im)",
+        "('[2p,Sg]'):(i)",
+        "('[3p,Sg]'):(i)",
+        "('[1p,Pl]'):(im)",
+        "('[2p,Pl]'):(id)",
+        "('[3p,Pl]'):(id)",
+    ]))
+
+
+# fsts['person'] = 
+
+fsts['lexicon'] = FST.re('$inf_vocab $tags', fsts)
+fsts['present'] = FST.re("($inf_vocab) ('[Present]':'') ($present_person_ending)", fsts)
+fsts['grammar'] = FST.re('$lexicon @ ($present)', fsts)
+
+print(Paradigm(fsts['grammar'], ".*"))
