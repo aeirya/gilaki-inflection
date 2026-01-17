9045d7cce7d7c2d5437e0de11a20f4dcb5d3288c
aeirya <aeirya@gmail.com>
2026-01-17 01:55:17 +0100

    add normalize rules

--- gltk/reader.py
+++ gltk/reader.py
@@ -3,6 +3,7 @@ from gltk.std import _symbols
 
 # insert space before and after punctuation to make split easier
 def _insert_punc_spaces():
+    # todo: use this in the grammar
     symbols_re = '|'.join(f"'{x}'" for x in _symbols)
 
     fsts = {}
@@ -15,7 +16,25 @@ def _insert_punc_spaces():
 
 insert_punc_spaces = _insert_punc_spaces()
 
+
+def _normalize_characters():
+    fsts = {
+        'erule': FST.re("$^rewrite((a '-́'):'á')"),
+        # 'newline': FST.re("$^rewrite('\n':'')"), # debug this
+    }
+    grammar = FST.re(".* @ $erule", fsts)
+    
+    return lambda text: list(
+        grammar.generate(
+            text#.lower()
+            ))[0]
+
+normalize_characters = _normalize_characters()
+
+
 def read_lines(filename):
     with open(filename) as file:
         for line in file.readlines():
-            yield insert_punc_spaces(line)
+            line = insert_punc_spaces(line)
+            line = normalize_characters(line)
+            yield line
